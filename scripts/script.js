d2c526a05f494e3a51e172bae722f83afd89f2fd
Josef K <ahmed.abshir91@gmail.com>
2023-10-29 00:43:28 +0100

    Updates index.js - getPasswordOptions function now prompts user to select desired character types

--- scripts/script.js
+++ scripts/script.js
@@ -89,18 +89,43 @@ var upperCasedCharacters = [
 ];
 
 //new array to hold the characters of all other arrays
-var allCharacters = specialCharacters.concat(numericCharacters).concat(lowerCasedCharacters).concat(upperCasedCharacters);
+var selectedChars = [];
+
+var charTypesArray = [specialCharacters, numericCharacters, lowerCasedCharacters, upperCasedCharacters];
+var chartypesNames = ['Special Characters', 'Numbers', 'Lowercased Letters', 'Uppercased Letters'];
+
+var numberOfTypesSelected = 0;
 
 // Function to prompt user for password options
 
 function getPasswordOptions() {
   var validEntry = false; //variable to be used as condition in while loop
 
+  alert('Please select at least 1 of the following 4 character types for your password');
+
+  for(var j=0; j<charTypesArray.length; j++){
+      
+    var questionTypes = confirm('Would you like the password to include ' + chartypesNames[j].toUpperCase() + '? Click OK for yes or CANCEL for no');
+    
+    if(questionTypes === true){
+      selectedChars = selectedChars.concat(charTypesArray[j]);
+      numberOfTypesSelected++;
+    }
+  }
+
+  if(numberOfTypesSelected < 1){
+    alert('You must select at least 1 character type. Please try again');
+    return;
+  }
+
+  numberOfTypesSelected = 0;
+
   //while loop runs as validEntry is false - as long as a number between 8 and 128 is not entered by user
   while(validEntry == false){
     //prompts user to enter desired password length and stores entry in the variable 'passwordLength'
     var passwordLength = prompt('How long would you like your password to be? Enter a number from 8 to 128 (including 8 and 128)');
     var parsedPasswordLength = parseInt(passwordLength); //converts user entry (string) to number 
+ 
 
     //conditional statement checks if number entered is between 8 and 128 
     if(parsedPasswordLength >= 8 && parsedPasswordLength <= 128 ){
@@ -113,20 +138,24 @@ function getPasswordOptions() {
     else{
       alert('Your entry is invalid. Please enter a number between 8 and 128'); //criteria not met - loop while execute again
     }
+
   }
+
+  
 
 return parsedPasswordLength;
 } 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-var randomIndex = Math.floor(Math.random() * arr.length);
+var randomIndex = Math.floor(Math.random() * arr.length); //random number generated (beas)
   
 return (arr.at(randomIndex));
 }
 
 var newPassword = [];
 var newPasswordString;
+
 // Function to generate password with user input
 function generatePassword() {
 
@@ -134,7 +163,7 @@ function generatePassword() {
 
   for(var i=0; i<lengthOfPassword; i++){
     
-    var currentChar = getRandom(allCharacters);
+    var currentChar = getRandom(selectedChars);
     newPassword.push(currentChar);
 
   }
@@ -154,9 +183,13 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writeNewPassword = () => {
   writePassword();
+  console.log('Before: ' + selectedChars)
+  selectedChars.length = 0;
+  console.log('After: ' + selectedChars);
 });
